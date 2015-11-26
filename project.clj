dac6c124999bcf88a678410d6171a5a0ee98f08e
Adrian Marina <adrianmarina.adrian@gmail.com>
2015-11-26 22:18:28 +0000

    upgrading dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring "1.2.0"]
-                 [ring-server "0.2.8" :exclusions [[org.clojure/clojure]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring "1.4.0"]
+                 [ring-server "0.4.0" :exclusions [[org.clojure/clojure]
                                                    [ring]]]
-                 [ring/ring-json "0.2.0"]
-                 [compojure "1.1.5" :exclusions [[org.clojure/clojure] [ring/ring-core]]]]
+                 [ring/ring-json "0.4.0"]
+                 [compojure "1.4.0" :exclusions [[org.clojure/clojure] [ring/ring-core]]]]
   :local-repo "lib")
